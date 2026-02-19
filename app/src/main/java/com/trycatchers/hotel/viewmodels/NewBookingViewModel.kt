47abc87345188794a6c0bebc32f94d92b4745812
Mateo Manchola <mmanchola12temporal@gmail.com>
2026-02-19 22:09:45 +0100

    android: remove obsolete NewBooking components - replaced by modular flow

--- app/src/main/java/com/trycatchers/hotel/viewmodels/NewBookingViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.trycatchers.hotel.viewmodels
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class NewBookingViewModel @Inject constructor() : ViewModel()
